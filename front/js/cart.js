e17731bf4d6bba2f4783afe525b3dbdbfb94a42f
WalidAFB <walidafbdev@gmail.com>
2022-12-03 04:08:26 +0100

    set item localstorage

---
+++ front/js/cart.js
@@ -0,0 +1,5 @@
+const item = JSON.parse(window.localStorage.getItem("itemArr"));
+const color = item.color
+const id = item.url
+const quantity = item.quantity
+console.log(item)
--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,5 @@
 const queryString = new URLSearchParams(window.location.search)
-let url = queryString.get("id")
+const url = queryString.get("id")
 
 fetch(`http://localhost:3000/api/products/${url}`)
 .then (res => res.json())
@@ -17,10 +17,24 @@ fetch(`http://localhost:3000/api/products/${url}`)
     description.innerHTML = data.description
 
     let colors = document.getElementById("colors");
-    let myHTML = ""
+    let myHTML = `<option value="">--SVP, choisissez une couleur --</option>`
     for (let i = 0; i < data.colors.length; i++) {
         myHTML += 
         `<option value="${data.colors[i]}">${data.colors[i]}</option>`
     }
     colors.innerHTML = myHTML
 })
+
+const addToCart = document.getElementById("addToCart")
+addToCart.addEventListener("click", () =>  {
+    const color = document.getElementById("colors").value
+    const quantity = document.getElementById("quantity").value
+    const product = {url: url, color: color, quantity: quantity}
+    if (localStorage.getItem("itemArr") == null) {
+        localStorage.setItem("itemArr", "[]")
+    }
+
+    let oldItemArr = JSON.parse(localStorage.getItem("itemArr"))
+    oldItemArr.push(product)
+    localStorage.setItem("itemArr", JSON.stringify(oldItemArr))
+    })
